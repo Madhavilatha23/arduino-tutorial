8e2de13d384e898f10387e81f3ba2afceba4d27b
Madhavi latha <madhavilathamalapati76@gmail.com>
2024-09-03 09:35:13 +0530

    Add files via upload

---
+++ ServoRotate-Userinput.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+Servo myservo;
+int val,ServoPin=9,angle=0;
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(9600);
+   myservo.attach(ServoPin);
+   Serial.println("Enter inputs a/b/c/d:");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(Serial.available())
+{
+  int inByte= Serial.read();
+  switch(inByte){
+    case'a':
+    angle=0;
+    break;
+    case'b':
+    angle=60;
+    break;
+  
+   case'c':
+    angle=90;
+    break;
+    case'd':
+    angle=120;
+    break;
+  }
+  Serial.println(angle);
+  }
+  myservo.write(angle);
+  delay(100);
+}
